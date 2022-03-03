0885eb3767907690ec4b794e53103d38aba6fe11
yami <yangming.yami@bytedance.com>
2022-03-03 13:15:21 +0800

    fix: 更改api路径

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,9 +1,9 @@
-export const SERVER_URL = '';
+export const SERVER_URL = '/api';
 // export const SERVER_URL = 'http://localhost:7001';
 
 const PATH: { [key: string]: string } = {
-  getSessionList: '/api/chat/sessions/',
-  sendMessage: '/api/message/send', // POST
+  getSessionList: '/chat/sessions/',
+  sendMessage: '/message/send', // POST
 };
 
 const API: { [key: string]: string } = {};
