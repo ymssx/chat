61d8f3244732ba3e5583a6b388794c8decd691d9
yami <yamiiyang@tencent.com>
2024-06-01 01:23:50 +0800

    fix: 修改路由

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,14 +1,14 @@
-export const SERVER_URL = '/';
-// export const SERVER_URL = 'http://localhost:7001';
+// export const SERVER_URL = '/api';
+export const SERVER_URL = '';
 
 const PATH: { [key: string]: string } = {
-  getSessionList: 'api/chat/sessions/',
-  sendMessage: 'api/message/send', // POST
+  getSessionList: '/chat/sessions/',
+  sendMessage: '/message/send', // POST
 };
 
 const API: { [key: string]: string } = {};
 for (const key in PATH) {
-  API[key] = SERVER_URL + PATH[key];
+  API[key] = SERVER_URL + '/api' + PATH[key];
 }
 
 export default API;
